396ea0895c9185e8054208cae586322cf38fb906
chenyinl <linchenyin@gmail.com>
2014-03-25 17:46:29 -0700

    Create Texts.js

---
+++ Texts.js
@@ -0,0 +1,302 @@
+(function() {
+Texts = function() {}
+var p = Texts.prototype;
+p.generic={
+    "magicMirror": [
+        {
+            "text": "Hi Jack.",
+            "delay": 1.5
+        },
+        {
+            "text": "I am filling out an app.",
+            "delay": 2
+        },
+        {
+            "text": "Are you finished school?",
+            "delay": 1.5
+        },
+        {
+            "text": "No. Not yet.",
+            "delay": 1.5
+        },
+        {
+            "text": "I see",
+            "delay": 3
+        }
+    ],
+    "trappedInACave": [
+        {
+            "text": "How is your",
+            "delay": "3"
+        },
+        {
+            "text": "day going?",
+            "delay": "3"
+        },
+        {
+            "text": "Quiet busy.",
+            "delay": 2.5
+        },
+        {
+            "text": "I am preparing",
+            "delay": 1.5
+        },
+        {
+            "text": "for my presentation",
+            "delay": 1.3
+        },
+        {
+            "text": "tomorrow on our",
+            "delay": 1.5
+        },
+        {
+            "text": "marketing strategy.",
+            "delay": "1.5"
+        }
+    ],
+    "example 3": [
+        {
+            "text": "Are you busy?",
+            "delay": 1.5
+        },
+        {
+            "text": "Quite busy.,",
+            "delay": 1.5
+        },
+        {
+            "text": "I am playing ",
+            "delay": 2.5
+        },
+        {
+            "text": "pool with my friend",
+            "delay": 1.5
+        },
+        {
+            "text": "at a pool hall",
+            "delay": 1.5
+        },
+        {
+            "text": "now!",
+            "delay": 3
+        }
+    ]
+};
+
+p.personal={
+    "whatMoreCouldYouWishFor": [
+        {
+            "text": "Hey, it's",
+            "delay": 1.5
+        },
+        {
+            "text": "%name%!",
+            "delay": 3.5
+        },
+        {
+            "text": "What more",
+            "delay": 1.5
+        },
+        {
+            "text": "could you wish for?",
+            "delay": 3
+        }
+    ],
+    "PlayingPool": [
+        {
+            "text": "I did not know,",
+            "delay": 1.5
+        },
+        {
+            "text": "you play pool.",
+            "delay": 1.5
+        },
+        {
+            "text": "Are you having fun?",
+            "delay": 1.5
+        },
+        {
+            "text": "I am having ",
+            "delay": 2.5
+        },
+        {
+            "text": "a great time.",
+            "delay": 1.5
+        },
+        {
+            "text": "How about you?",
+            "delay": 3.5
+        },
+        {
+            "text": "What are you",
+            "delay": 1.5
+        },
+        {
+            "text": "doing?",
+            "delay": 3.5
+        }
+    ]
+};
+
+p.group={
+    "homework": [
+        {
+            "text": "I am taking",
+            "delay": 1.5
+        },
+        {
+            "text": "a break from my",
+            "delay": 3.5
+        },
+        {
+            "text": "homework.",
+            "delay": 1.5
+        },
+        {
+            "text": "Really?",
+            "delay": 3
+        }
+    ]
+};
+
+p.firstTimeVisitor={
+    "kidInACandyStore": [
+        {
+            "text": "%name%!",
+            "delay": 3.5
+        },
+        {
+            "text": "It’s your first",
+            "delay": 1.5
+        },
+        {
+            "text": "time here.",
+            "delay": 2.5
+        },
+        {
+            "text": "I hope you feel",
+            "delay": 1.5
+        },
+        {
+            "text": "like Abu in the ",
+            "delay": 1.5
+        },
+        {
+            "text": "Cave of Wonders.",
+            "delay": 1.5
+        }
+    ]
+};
+
+p.birthday={
+    "complimentGenie": [
+        {
+            "text": "%name%,",
+            "delay": 3.5
+        },
+        {
+            "text": "Happy birthday!",
+            "delay": 3
+        },
+        {
+            "text": "I’ll give you",
+            "delay": 1.5
+        },
+        {
+            "text": "the same compliment",
+            "delay": 1.5
+        },
+        {
+            "text": "another Genie",
+            "delay": 1.5
+        },
+        {
+            "text": "once gave me:",
+            "delay": 2.5
+        },
+        {
+            "text": "you don’t look",
+            "delay": 1.5
+        },
+        {
+            "text": "a century",
+            "delay": 1.5
+        },
+        {
+            "text": "over a thousand!",
+            "delay": 3
+        }
+    ],
+    "lifetimeSupplyOfChurros": [
+        {
+            "text": "Is it really",
+            "delay": 1.5
+        },
+        {
+            "text": "your birthday,",
+            "delay": 1.5
+        },
+        {
+            "text": "%name%?",
+            "delay": 3.5
+        },
+        {
+            "text": "Abu would",
+            "delay": 2
+        },
+        {
+            "text": "love to help you",
+            "delay": 2
+        },
+        {
+            "text": "blow out the candles",
+            "delay": 2
+        },
+        {
+            "text": "on your cake.",
+            "delay": 3
+        }
+    ]
+};
+
+p.weddingoccasion={
+    "neverThisHappyAgain": [
+        {
+            "text": "%names%",
+            "delay": 3.5
+        },
+        {
+            "text": "Congratulations",
+            "delay": 1.5
+        },
+        {
+            "text": "to the happy couple.",
+            "delay": 2.5
+        },
+        {
+            "text": "Forever",
+            "delay": 1.5
+        },
+        {
+            "text": "is a long time - ",
+            "delay": 2
+        },
+        {
+            "text": "be glad you don't",
+            "delay": 1.5
+        },
+        {
+            "text": "have to spend it",
+            "delay": 1.5
+        },
+        {
+            "text": "cooped up",
+            "delay": 1.5
+        },
+        {
+            "text": "in this lamp.",
+            "delay": 3
+        }
+    ]
+};
+
+}());
